ba74506ce9a6e2c3f9cb0f4f76a33910c6e8d64a
Jared Carey <jared.carey@elastic.co>
2019-07-30 15:30:47 -0600

    fix comment

--- uploader/euploader.go
+++ uploader/euploader.go
@@ -6,7 +6,7 @@ import (
 	"github.com/elastic/eluploader/cmd/eluploader/commands"
 )
 
-// RunUpload is used for the Elastic upload service when the `-u {{ upload uui }}` is present
+// RunUpload is used for the Elastic upload service when the `-U {{ upload uuid }}` is present
 func RunUpload(filePath, uploadUID string) {
 	apiURL := "https://upload-staging.elstc.co"
 	numWorkers := runtime.NumCPU()
