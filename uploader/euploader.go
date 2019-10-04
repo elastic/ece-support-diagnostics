7199f0b011c84a3a800037d276f07af6383e801d
Jared Carey <jared.carey@elastic.co>
2019-10-04 00:11:24 -0600

    elastic uploader production url

--- uploader/euploader.go
+++ uploader/euploader.go
@@ -8,7 +8,7 @@ import (
 
 // RunUpload is used for the Elastic upload service when the `-U {{ upload uuid }}` is present
 func RunUpload(filePath, uploadUID string) {
-	apiURL := "https://upload-staging.elstc.co"
+	apiURL := "https://upload.elastic.co"
 	numWorkers := runtime.NumCPU()
 	cmd := &commands.UploadCmd{UploadID: uploadUID, Filepath: filePath, ApiURL: apiURL, NumWorkers: numWorkers}
 	cmd.Execute()
